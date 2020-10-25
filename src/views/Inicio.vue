ccdd52434fe51e071029c2a9e227fea7d8a9a562
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-10-25 13:19:57 +0100

    New view for initial screen

---
+++ src/views/Inicio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Inicio</p>
+    <p @click="Reset" style="cursor: pointer">Reset</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Inicio",
+  methods: {
+    async Reset() {
+      await this.$store.dispatch("reset", {});
+    },
+  },
+};
+</script>
