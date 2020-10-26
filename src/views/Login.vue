d08c1c956d49514d3e3c4f3790bd18ec415201dd
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-10-26 18:05:48 +0100

    Center elements and put text to left

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>   
+  <div>
     <div class="flex items-center flex-col align-middle h-auto min-h-screen">
-      <div class="h-4 sm:h-8 md:h-10 lg:h-12 xl:h-14 w-full bg-blue-800 border-t-40 border-gray-600">
-      </div>
-      <img class="ml-12 mt-4 object-contain  sm:h-8 md:h-10 lg:h-12 xl:h-14 w-full object-left" src="../assets/images/logo-uah.png" alt="Logo UAH">
       <div
-        class="z-20 flex flex-col justify-center align-middle self-center w-11/12 md:w-7/12 my-12 py-12 px-12 card border-gray-500 border rounded-md"
+        class="h-12 sm:h-12 md:h-14 w-screen bg-blue-800 border-t-40 border-gray-600 min-w-full"
+      ></div>
+      <img
+        class="ml-12 mt-4 h-12 object-contain  xl:h-14 w-full object-left"
+        src="../assets/images/logo-uah.png"
+        alt="Logo UAH"
+      />
+      <div
+        class="z-20 flex flex-col justify-center align-middle self-center w-11/12 md:w-4/12 my-12 py-12 px-12 card border-gray-500 border rounded-md"
       >
         <p
           class="text-gray-900 mx-5 font-bold text-left pt-2 text-base sm:text-lg md:text-2xl lg:text-2xl mb-2"
@@ -14,38 +19,50 @@
         </p>
 
         <p
-          class="text-gray-700 mx-5 text-left  text-base sm:text-sm md:text-md lg:text-md mb-4"
+          class="text-gray-700 mx-5 text-left text-base sm:text-sm md:text-md lg:text-md mb-4"
         >
           Introduce el nombre de usuario y la contraseña para la conexión.
         </p>
+        <div class="mx-5">
+          <div class="grid grid-rows-1 grid-flow-row text-left">
+            <label
+              for="email"
+              class="text-gray-900 font-semibold text-left pt-2 text-base sm:text-lg md:text-xl lg:text-xl mb-2"
+            >
+              Usuario
+            </label>
+          </div>
+          <div class="grid grid-rows-1 grid-flow-row text-left">
+            <input
+              id="email"
+              v-model="email"
+              type="text"
+              class="bg-white focus:outline-none self-center text-center w-full border focus:border-blue-900 py-2 block appearance-none leading-normal"
+              @:keyup.enter="login"
+            />
+          </div>
+        </div>
 
-        <label
-          for="email"
-          class="text-gray-900 font-semibold text-left mx-5 pt-2 text-base sm:text-lg md:text-xl lg:text-xl mb-2"
-        >
-          Usuario
-        </label>
-        <input
-          id="email"
-          v-model="email"
-          type="text"
-          class="bg-white focus:outline-none self-center text-center w-11/12 border focus:border-blue-900 py-2 block appearance-none leading-normal"
-          @:keyup.enter="login"
-        />
+        <div class="mx-5">
+          <div class="grid grid-rows-1 grid-flow-row text-left">
+            <label
+              for="password"
+              class="text-gray-900 font-semibold text-left pt-2 text-base sm:text-lg md:text-xl lg:text-xl mb-2"
+            >
+              Contraseña
+            </label>
+          </div>
+          <div class="grid grid-rows-1 grid-flow-row text-left">
+            <input
+              id="password"
+              v-model="password"
+              type="password"
+              class="bg-white focus:outline-none self-center text-center w-full border focus:border-blue-900 py-2 block appearance-none leading-normal"
+              @:keyup.enter="login"
+            />
+          </div>
+        </div>
 
-        <label
-          for="password"
-          class="text-gray-900 font-semibold text-left mx-5 pt-2 text-base sm:text-lg md:text-xl lg:text-xl mb-2"
-        >
-          Contraseña
-        </label>
-        <input
-          id="password"
-          v-model="password"
-          type="password"
-          class="bg-white focus:outline-none self-center text-center w-11/12 focus:shadow-outline border border-gray-300 py-2 block appearance-none leading-normal"
-          @:keyup.enter="login"
-        />
         <div>
           <button
             class="bg-blue-800 hover:bg-blue-900 text-white font-bold w-11/12 mt-4 py-2 px-4"
@@ -89,7 +106,7 @@ export default {
 
 <style scoped>
 .card {
-  background-color: #fafafa;
+  background-color: #fff;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 }
