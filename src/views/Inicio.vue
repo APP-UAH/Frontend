18d5db47f4419dc127758b0ea6f48711ee72b968
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-11-03 19:46:59 +0100

    Fix grid

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <p>Inicio</p>
+  <div class="h-auto min-h-screen inicio">
+    <div
+      class="h-12 sm:h-12 md:h-14 w-screen bg-blue-800 border-t-40 border-gray-600 min-w-full"
+    ></div>
+    <img
+      class="ml-12 mt-4 h-12 object-contain xl:h-14 w-full object-left"
+      src="../assets/images/logo-uah.png"
+      alt="Logo UAH"
+    />
     <p @click="Reset" style="cursor: pointer">Reset</p>
-    <div class="grid grid-rows-3 grid-flow-col gap-4 w-full container">
-      <div v-if="this.tipoUsuario == 'administracion'" >
-        <div v-for="action in accionesAdmin" :key=action >
-            <TarjetaInicio :title="action.title" :route="action.route"></TarjetaInicio>
+    <div class="w-full container">
+      <div
+        v-if="this.tipoUsuario == 'administracion'"
+        class="grid grid-cols-3 gap-12"
+      >
+        <div v-for="action in accionesAdmin" :key="action">
+          <div class="col-span-1">
+            <TarjetaInicio
+              :title="action.title"
+              :route="action.route"
+            ></TarjetaInicio>
+          </div>
         </div>
       </div>
       <div v-else>
-        <div v-for="titulos in acciones" :key=titulos>
-            <TarjetaInicio :title="titulos"></TarjetaInicio>
+        <div v-for="titulos in acciones" :key="titulos">
+          <TarjetaInicio :title="titulos"></TarjetaInicio>
         </div>
       </div>
     </div>
@@ -18,27 +33,39 @@
 </template>
 
 <script>
-import TarjetaInicio from "../components/tarjetaInicio"
+import TarjetaInicio from "../components/tarjetaInicio";
 export default {
   name: "Inicio",
   components: {
-    TarjetaInicio
+    TarjetaInicio,
   },
-  data(){
-    return { accionesAdmin: [{title:"Reservar aulas", route:"/prueba1" },{title:"Ver informes", route:"/prueba2"},{title:"Aceptar reservas", route:""}],
-              acciones:["Reservar aulas", "Ver calendario"],
-              tipoUsuario: this.$store.getters.getUser.role}
+  data() {
+    return {
+      accionesAdmin: [
+        { title: "Reservar aulas", route: "/prueba1" },
+        { title: "Ver informes", route: "/prueba2" },
+        { title: "Aceptar reservas", route: "" },
+      ],
+      acciones: ["Reservar aulas", "Ver calendario"],
+      tipoUsuario: this.$store.getters.getUser.role,
+    };
   },
   methods: {
     async Reset() {
-      await this.$store.dispatch("reset", {});
+      await this.$store
+        .dispatch("reset", {})
+        .then(() => this.$router.push("/login"));
     },
   },
 };
 </script>
 
-
 <style scoped>
+
+.inicio{
+  overflow: hidden;
+}
+
 .card {
   background-color: #fff;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
