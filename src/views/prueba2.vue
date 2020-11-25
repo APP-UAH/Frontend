55c2bcb28f43c716f954c6feed37927726309d8f
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-11-25 20:30:04 +0100

    🚧  This is for dev only, in future we'll use another thing

--- src/views/prueba2.vue
+++ src/views/prueba2.vue
@@ -1,3 +1,43 @@
 <template>
-    <p> Prueba 1 </p>
+  <input type="file" @change="previewFiles" />
 </template>
+
+<script>
+import Papa from "papaparse";
+export default {
+  data() {
+    return {
+      config: {
+        header: true,
+      },
+    };
+  },
+  methods: {
+    async previewFiles(event) {
+      let csv = await this.processCSVFile(event.target.files[0]);
+      this.$router.push({
+        name: "informe",
+        params: {
+          csv: csv,
+          title:
+            "Reserva NL5, Salvador Otón, 26/11/2020 17:00",
+        },
+      });
+    },
+    processCSVFile(csv) {
+      return new Promise((resolve, reject) => {
+        Papa.parse(csv, {
+          header: true,
+          worker: true,
+          error: () => {
+            console.log(reject);
+          },
+          complete: (results) => {
+            resolve(results.data);
+          },
+        });
+      });
+    },
+  },
+};
+</script>
